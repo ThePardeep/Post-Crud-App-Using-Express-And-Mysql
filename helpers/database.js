f0ab11873c25f302af766f11604a4c0b40f7ec96
0 <pardeepsaini54321@gmail.com>
2018-12-23 22:59:23 +0530

    Initial Change

--- helpers/database.js
+++ helpers/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV  === 'production') {
             module.exports = {
-                host: 'ck.codingkeeda.com:3306',
+                host: 'localhost',
                 user: 'CK_APP',
                 password: "CK_APP&@",
                 database: "ckapp"
