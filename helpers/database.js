2ac45e212e8bce7886881c91b1019f7139a06c2a
Pardeep Saini <40034471+ThePardeep@users.noreply.github.com>
2019-10-13 00:06:41 +0530

    Update database.js

--- helpers/database.js
+++ helpers/database.js
@@ -1,10 +1,10 @@
 if (process.env.NODE_ENV  === 'production') {
             module.exports = {
-                host: '127.0.0.1',
-                user: 'CK_APP',
-                password: "CK_APP&@",
-                database: "ckapp"
-            }
+    host: process.env.HOST,
+    user: process.env.DU,
+    password: process.env.DP,
+    database: process.env.DBNAME
+  };
         } else {
             module.exports = {
                 host: '127.0.0.1',
